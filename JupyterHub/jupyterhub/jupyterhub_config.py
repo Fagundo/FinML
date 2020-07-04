f8061e53ec6e0255b5b3c4e7e6d4628588ca9b03
Michael Fagundo <mjfagundo@gmail.com>
2020-07-03 20:13:26 -0400

    Configuring JupyterHub and Traefik

--- JupyterHub/jupyterhub/jupyterhub_config.py
+++ JupyterHub/jupyterhub/jupyterhub_config.py
@@ -1,43 +1,25 @@
 # JupyterHub configuration
-#
-## If you update this file, do not forget to delete the `jupyterhub_data` volume before restarting the jupyterhub service:
-##
-##     docker volume rm jupyterhub_jupyterhub_data
-##
-## or, if you changed the COMPOSE_PROJECT_NAME to <name>:
-##
-##    docker volume rm <name>_jupyterhub_data
-##
 
 import os
+from dockerspawner import SwarmSpawner
 
 ## Generic
-c.JupyterHub.admin_access = True
 c.Spawner.default_url = '/lab'
 
-## Authenticator
-from jhub_cas_authenticator.cas_auth import CASAuthenticator
-c.JupyterHub.authenticator_class = CASAuthenticator
+# NativeAuthenticator https://github.com/jupyterhub/nativeauthenticator
+c.JupyterHub.authenticator_class = 'nativeauthenticator.NativeAuthenticator'
 
-# The CAS URLs to redirect (un)authenticated users to.
-c.CASAuthenticator.cas_login_url = 'https://cas.uvsq.fr/login'
-c.CASLocalAuthenticator.cas_logout_url = 'https://cas.uvsq/logout'
-
-# The CAS endpoint for validating service tickets.
-c.CASAuthenticator.cas_service_validate_url = 'https://cas.uvsq.fr/serviceValidate'
-
-# The service URL the CAS server will redirect the browser back to on successful authentication.
-c.CASAuthenticator.cas_service_url = 'https://%s/hub/login' % os.environ['HOST']
-
-c.Authenticator.admin_users = { 'lucadefe' }
-
+# Set admin user
+c.Authenticator.admin_users = {'fagundo'}
+c.DummyAuthenticator.password = 'password'
 
 ## Docker spawner
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.image = os.environ['DOCKER_JUPYTER_CONTAINER']
 c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
-# See https://github.com/jupyterhub/dockerspawner/blob/master/examples/oauth/jupyterhub_config.py
+
 c.JupyterHub.hub_ip = os.environ['HUB_IP']
+c.JupyterHub.port = 80
 
 # user data persistence
 # see https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
